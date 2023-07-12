8aa1283421af376452353a34c3ee5ac9ea003763
guru8848digital <guruprasad@8848digital.com>
2023-07-12 13:34:03 +0530

    [fix][hide_&set_null_asset_if_asset_expense_not_checked]

--- jfs_asset_customization/public/js/journal_entry.js
+++ jfs_asset_customization/public/js/journal_entry.js
@@ -1,7 +1,14 @@
 frappe.ui.form.on('Journal Entry', {
+    refresh:function(frm){
+        frm.set_df_property("asset","hidden",1)
+        if(frm.doc.asset_expense){
+            frm.set_df_property("asset","hidden",0)
+        }
+    },
     asset_expense: function(frm){
         if(frm.doc.asset_expense){
             frm.set_df_property('asset', "reqd", 1);
+            frm.set_df_property('asset', "hidden", 0);
             cur_frm.fields_dict['asset'].get_query = function(doc) {
                 return {
                     filters: {
@@ -12,7 +19,9 @@ frappe.ui.form.on('Journal Entry', {
             }
         }
         else{
+            frm.set_value("asset",null)
             frm.set_df_property('asset', "reqd", 0);
+            frm.set_df_property('asset', "hidden", 1);
         }
     }
 });
